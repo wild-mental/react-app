888e1de25b3055a4d4d6f4255dd087c371b7378b
wild-mental <wild.mental@kidsnote.com>
2024-07-08 13:28:11 +0900

    App 에서 ExpenseList 로드 및 동작 확인

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,11 @@ import React, {useState} from 'react';
 import 'App.css';
 import styled from 'styled-components';
 import ExpenseForm, {ExpenseData} from "./components/ExpenseForm";
+import ExpenseItem, {ExpenseItemProps} from "./components/ExpenseItem";
 import EventBinderButton from "components/EventBinderButton";
 import DrillingParent from "components/DrillingParent";
 import CalledBackParent from "./components/CalledBackParent";
+import ExpenseList from "./components/ExpenseList";
 
 interface ButtonProps {
     primary?: boolean;
@@ -27,24 +29,43 @@ const StyledButton = styled.button<ButtonProps>`
 
 function App() {
     // App 자체에서 지출데이터의 목록을 배열로 관리한다.
-    const [expenses, setExpenses] = useState<ExpenseData[]>([]);
+    const [expenses, setExpenses] = useState<ExpenseItemProps[]>(
+        [
+            {
+                title: '치킨먹음',
+                price: 30000,
+                date: new Date(2024, 6 - 1, 3)
+            },
+            {
+                title: '족발먹음',
+                price: 40000,
+                date: new Date(2023, 12 - 1, 17)
+            },
+            {
+                title: '헬스장등록',
+                price: 300000,
+                date: new Date(2024, 6 - 1, 12)
+            }
+        ]
+    );
     // add 이벤트 발생 시 데이터의 배열에 맨 첫 항목으로 추가된 지출 내역을 넣는다.
-    const addExpenseHandler = (expense: ExpenseData) => {
+    const addExpenseHandler = (expense: ExpenseItemProps) => {
         setExpenses(prevExpenses => [expense, ...prevExpenses])
         console.log(expenses, expense);
     }
     return (
         <div className="App">
             {/*<AppHeader />*/}
-            <StyledButton primary>Primary Button</StyledButton>
-            <StyledButton>Default button</StyledButton>
-            <br/><hr/>
-            <EventBinderButton />
-            <hr/>
-            <DrillingParent name={"this is to check props drilling"} />
-            <hr/>
-            <CalledBackParent />
+            {/*<StyledButton primary>Primary Button</StyledButton>*/}
+            {/*<StyledButton>Default button</StyledButton>*/}
+            {/*<br/><hr/>*/}
+            {/*<EventBinderButton />*/}
+            {/*<hr/>*/}
+            {/*<DrillingParent name={"this is to check props drilling"} />*/}
+            {/*<hr/>*/}
+            {/*<CalledBackParent />*/}
             <ExpenseForm onSaveExpense={addExpenseHandler} />
+            <ExpenseList items={expenses} />
         </div>
     );
 }
